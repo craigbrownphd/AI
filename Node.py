317aa3a26fa014fb25d5aacb5a59a5e407871588
himanshuo <ho2es@virginia.edu>
2015-09-11 00:06:11 -0400

    parsing

---
+++ Node.py
@@ -0,0 +1,50 @@
+__author__ = 'himanshu'
+
+
+
+
+class Node(object):
+    """
+    If value is not [! & |] then it is a leaf.
+    """
+    def __init__(self, left, value, right):
+        assert isinstance(left, Node) or left is None
+        assert isinstance(right, Node) or right is None
+        assert isinstance(value, str) and len(value) is 1
+
+        if value not in ['!', '&', '|']:
+            assert left is None
+            assert right is None
+
+        if value is '!':
+            """
+            for !, our convention is to make left the notted value. right is None
+            """
+            assert left is not None and right is None
+        elif value in ['&', '|']:
+            assert left is not None and right is not None
+
+
+
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+        #todo: there are requirements for what characters are allowed. a-z, A-Z, underscores,...
+
+
+    def __eq__(self, other):
+        assert isinstance(other, Node)
+
+        if other is None:
+            return False
+
+        # by memory address
+        if other is self:
+            return True
+
+        return self.left == other.left and \
+               self.right == other.right and \
+               self.value == other.value
+
---
+++ test_parse.py
@@ -0,0 +1,111 @@
+from unittest import TestCase
+from parser import parse, char_index
+from Node import Node
+
+
+# ! & |
+class TestParse(TestCase):
+
+    def test_parse_empty(self):
+        self.assertEqual(None ,parse(''))
+        self.assertEqual(None, parse(None))
+
+    def test_parse_empty_parens(self):
+        self.assertEqual(None, parse('()'))
+        self.assertEqual(None, parse('(())'))
+        self.assertEqual(None, parse('((()))'))
+        # self.assertEqual(None, parse('(())()()()()()()()'))
+
+
+
+    def test_parse_simple_letter(self):
+        a = Node(None, 'a', None)
+        self.assertEqual(a, parse('a'))
+
+    def test_parse_simple_or(self):
+
+        a = Node(None, 'a', None)
+        b = Node(None, 'b', None)
+        or_node = Node(a, '|', b)
+
+        self.assertEqual(or_node, parse('a|b'))
+
+    def test_parse_simple_and(self):
+
+        a = Node(None, 'a', None)
+        b = Node(None, 'b', None)
+        and_node = Node(a, '&', b)
+
+        self.assertEqual(and_node, parse('a&b'))
+
+
+    def test_parse_simple_or_and(self):
+
+        a = Node(None, 'a', None)
+        b = Node(None, 'b', None)
+        or_node = Node(a, '|', b)
+        c = Node(None, 'c', None)
+        and_node = Node(or_node, '&', c)
+
+        self.assertEqual(and_node, parse('a|b&c'))
+
+    def test_parse_complex_or_and_not(self):
+        a = Node(None, 'a', None)
+        b = Node(None, 'b', None)
+        c = Node(None, 'c', None)
+        d = Node(None,'d',None)
+
+
+
+        not_node = Node(d, '!', None)
+
+        or_node_left = Node(a, '|', b)
+        or_node_right = Node(c, '|',not_node)
+        and_node = Node(or_node_left, '&', or_node_right)
+
+        self.assertEqual(and_node, parse('a|b&c|!d'))
+
+    def test_parse_simple_or_not_and(self):
+        a = Node(None, 'a', None)
+        b = Node(None, 'b', None)
+        c = Node(None, 'c', None)
+        d = Node(None,'d',None)
+
+
+        and_node = Node(c, '&', d)
+        not_node = Node(and_node, '!', None)
+        or_node_inner = Node(b, '|', not_node)
+        or_node = Node(a, '|',or_node_inner)
+
+        self.assertEqual(or_node, parse('a|b|!(c&d)'))
+
+
+    # todo:  a|(b&c)
+    # todo:  a|b&c
+    # todo:  (a|b)&c
+    # todo:  a|(b&(c|d))
+    # todo: !a & b
+    # todo: !(a&b)
+    # todo: a|b|c&d
+    # todo: a|b|c|d
+    # todo: !a|b|c|d
+
+    # todo: !a|b|c
+
+    # todo: !a|c
+    #todo: tests with spaces in expression
+
+
+    def test_char_index(self):
+        mydict = {
+            '!a':0,
+            '!(a&b)':0,
+            'a|b|c':1,
+            'a|b&c':3,
+            '!!!!a':0,
+            '!a|!b':2,
+            '(a|b)&c':5,
+            'a|(b&c)':1,
+        }
+        for k,v in mydict.items():
+            self.assertEquals(v, char_index(k), '{}->{}'.format(k,v))
